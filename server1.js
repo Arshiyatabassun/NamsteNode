28cf91bbfad997b75488f97e6a07db37aafb1f92
Arshiya Tabassum <96403942+Arshiyatabassun@users.noreply.github.com>
2025-11-19 22:31:01 +0530

    creating a server practice

---
+++ server1.js
@@ -0,0 +1,8 @@
+//Node Core Modules:http,https,os,path,zlib,fs,util
+const http =require("node:http");
+
+const server1=http.createServer(function(req,res){
+
+    res.end("Hello World")
+})
+server1.listen(7777);
