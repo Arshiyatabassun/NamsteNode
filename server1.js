096900d6999581df4fd3920adb36c2717a1dab99
Arshiya Tabassum <96403942+Arshiyatabassun@users.noreply.github.com>
2025-11-21 23:27:47 +0530

    nthng

--- server1.js
+++ server1.js
@@ -1,8 +1,9 @@
 //Node Core Modules:http,https,os,path,zlib,fs,util
-const http =require("node:http");
+// const http =require("node:http");
 
-const server1=http.createServer(function(req,res){
+// const server1=http.createServer(function(req,res){
 
-    res.end("Hello World")
-})
-server1.listen(7777);
+//     res.end("Hello World")
+// })
+// server1.listen(7777);
+
